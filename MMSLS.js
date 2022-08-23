d2fa1a08934ef88433ea62fe2634fcae6ae11bae
tlyps <57831134+tlyps@users.noreply.github.com>
2022-08-23 08:07:52 +0800

    Add files via upload

---
+++ MMSLS.js
@@ -0,0 +1,37 @@
+/**
+利用随机概率实现多账户运行
+
+填写格式如下：
+
+
+const MMSLS = [
+    
+    "https://www.maimemo.com/share/page?uid=12661190&pid=7d0b81c3770d01bad6969514b3f79e32&tid=fa1b766caa18cc4efcbe76e9dbf8e656",
+  ]
+=====================以上为范例！=====================
+
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+ */
+const MMSLS = [
+    
+    "https://www.maimemo.com/share/page?uid=12661190&pid=7d0b81c3770d01bad6969514b3f79e32&tid=fa1b766caa18cc4efcbe76e9dbf8e656",
+    "",
+    "",
+  ]
+  /**
+   * 生成随机数字
+   * @param {number} min 最小值（包含）
+   * @param {number} max 最大值（不包含）
+   */
+  function randomNumber(min = 0, max = 100) {
+    return Math.min(Math.floor(min + Math.random() * (max - min)), max);
+  }
+  const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
+  
+  module.exports = {
+    MMSL_random
+  }
